b1789f28de29107d76f624d9c9aa7e4bb0c85226
Bradley Gay <bradley.gay@aol.com>
2018-06-12 16:21:46 -0400

    Fourth Commit

---
+++ programs/arrayex3.rb
@@ -0,0 +1,5 @@
+arr = [["test", "hello", "world"], ["example", "mem"]]
+
+#Return the word "example" from the array
+
+arr.last.first
---
+++ programs/arrayex7.rb
@@ -0,0 +1,8 @@
+arr = [1, 2, 3, 4, 5]
+arr_2 = []
+p arr
+
+arr.each do |num|
+  arr_2.push(num + 2)
+end
+p arr_2
