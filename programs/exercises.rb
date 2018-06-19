2163622ec9806124462fb5ea908aa57188a937fe
Bradley Gay <bradley.gay@aol.com>
2018-06-19 08:27:42 -0400

    Ninth Commit

--- programs/exercises.rb
+++ programs/exercises.rb
@@ -74,9 +74,35 @@ end
 end
 =end
 
+=begin
 number = 0
 
 until number == 10
   number += 1
-  puts number
+  puts number.next if number.odd?
+end
+=end
+
+=begin
+number_a = 0
+number_b = 0
+
+loop do
+  number_a += rand(2)
+  number_b += rand(2)
+    next unless number_a == 5 || number_b == 5
+    puts "5 was reached!"
+    break
+end
+=end
+
+
+def greeting
+  puts 'Hello!'
+end
+
+number_of_greetings = 2
+while number_of_greetings > 0
+  greeting
+  number_of_greetings -= 1
 end
