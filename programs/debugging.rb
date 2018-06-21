2c7ce7f5a57938a0760e8ff788f5b06fdb4c387c
Bradley Gay <bradley.gay@aol.com>
2018-06-21 12:26:45 -0400

    Fourteenth Commit

---
+++ programs/debugging.rb
@@ -0,0 +1,149 @@
+=begin
+def find_first_nonzero_among(numbers)
+  numbers.each do |n|
+    p n if n.nonzero?
+  end
+end
+
+# Examples
+
+find_first_nonzero_among(0, 0, 1, 0, 2, 0)
+find_first_nonzero_among(1)
+=end
+
+=begin
+def multiply_by_five(n)
+  n * 5
+end
+
+puts "Hello! Which number would you like to multiply by 5?"
+number = gets.chomp.to_i
+
+puts "The result is #{multiply_by_five(number)}!"
+=end
+
+=begin
+pets = { cat: 'fluffy', dog: ['sparky', 'fido'], fish: 'oscar' }
+
+pets[:dog] << 'bowser'
+
+p pets #=> {:cat=>"fluffy", :dog=>"bowser", :fish=>"oscar"}
+=end
+
+=begin
+numbers = [5, 2, 9, 6, 3, 1, 8]
+
+even_numbers = numbers.select do |n|
+  n.even? 
+end
+
+p even_numbers
+=end
+
+=begin
+def get_quote(person)
+  if person == 'Yoda'
+    return 'Do. Or do not. There is no try.'
+  end
+
+  if person == 'Confucius'
+    return 'I hear and I forget. I see and I remember. I do and I understand.'
+  end
+
+  if person == 'Einstein'
+    return 'Do not worry about your difficulties in Mathematics. I can assure you mine are still greater.'
+  end
+end
+
+p 'Confucius says: ' + get_quote('Confucius')
+=end
+
+=begin
+balance = 0
+
+january = {
+  income: [ 1200, 75 ],
+  expenses: [ 650, 140, 33.2, 100, 26.9, 78 ]
+}
+
+february = {
+  income: [ 1200 ],
+  expenses: [ 650, 140, 320, 46.7, 122.5 ]
+}
+
+march = {
+  income: [ 1200, 10, 75 ],
+  expenses: [ 650, 140, 350, 12, 59.9, 2.5 ]
+}
+
+def calculate_balance(month)
+  plus  = month[:income].sum
+  minus = month[:expenses].sum
+
+  plus - minus
+end
+
+[january, february, march].each do |month| 
+  balance += calculate_balance(month)
+end
+
+puts balance
+=end
+
+=begin
+colors = ['red', 'yellow', 'purple', 'green', 'dark blue', 'turquoise', 'silver', 'black']
+things = ['pen', 'mouse pad', 'coffee mug', 'sofa', 'surf board', 'training mat', 'notebook']
+
+colors.shuffle!
+things.shuffle!
+
+i = 0
+loop do
+  break if i > things.length - 1 || i > colors.length - 1
+
+  if i == 0
+    puts 'I have a ' + colors[i] + ' ' + things[i] + '.'
+  else
+    puts 'And a ' + colors[i] + ' ' + things[i] + '.'
+  end
+  i += 1
+end
+=end
+
+=begin
+def digit_product(str_num)
+  digits = str_num.chars.map { |n| n.to_i }
+  product = 1
+  digits.each do |digit|
+    product *= digit
+  end
+
+  product
+end
+
+
+p digit_product('12345')
+# expected return value: 120
+# actual return value: 0
+=end
+
+# Each player starts with the same basic stats.
+
+player = { strength: 10, dexterity: 10, charisma: 10, stamina: 10 }
+
+# Then the player picks a character class and gets an upgrade accordingly.
+
+character_classes = {
+  warrior: { strength:  20 },
+  thief:   { dexterity: 20 },
+  scout:   { stamina:   20 },
+  mage:    { charisma:  20 }
+}
+
+puts 'Please type your class (warrior, thief, scout, mage):'
+input = gets.chomp.downcase.to_sym
+
+player.merge!(character_classes[input])
+
+puts 'Your character stats:'
+puts player
